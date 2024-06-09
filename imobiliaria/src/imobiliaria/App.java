24292271c3e25abb53f01e79884a042641639dfd
danieljoaquim <150199553+danieljoaquim@users.noreply.github.com>
2024-06-09 11:37:09 -0300

    Add files via upload

--- imobiliaria/src/imobiliaria/App.java
+++ imobiliaria/src/imobiliaria/App.java
@@ -1,17 +1,59 @@
 package imobiliaria;
+
 import java.util.Scanner;
 import classes.*;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        
-    	Scanner sc = new Scanner (System.in);
-    	Imobiliaria imobiliaria = new Imobiliaria();
-    	
-    	
-    	imobiliaria.adicionaImovel(sc);
-    	imobiliaria.listagemImoveis();
-    	imobiliaria.listagemPorCondicao(sc);
-    	imobiliaria.modificarImovel(sc);
-    	imobiliaria.removerImovel(sc);
-    }
+	public static void main(String[] args) throws Exception {
+
+		Scanner sc = new Scanner(System.in);
+		Imobiliaria imobiliaria = new Imobiliaria();
+
+		int opcao = 0;
+
+		
+		System.out.println("Bem vindo à imobiliária!");
+		System.out.println();
+		System.out.println("==========================");
+		System.out.println();
+		
+		do {
+			System.out.println("Digite 1 para adicionar um novo imóvel;");
+			System.out.println();
+			System.out.println("Digite 2 para listar os imóveis disponíveis;");
+			System.out.println();
+			System.out.println("Digite 3 para listar os imóveis de acordo com uma condição;");
+			System.out.println();
+			System.out.println("Digite 4 para modificar o preço de um um imóvel já existente;");
+			System.out.println();
+			System.out.println("Digite 5 para excluir um imóvel cadastrado;");
+			System.out.println();
+			System.out.println("Digite 0 para sair.");
+			System.out.println();
+			System.out.println("==========================");
+
+			opcao = sc.nextInt();
+
+			switch (opcao) {
+			case 1:
+				imobiliaria.adicionaImovel(sc);
+				break;
+			case 2:
+				imobiliaria.listagemImoveis();
+				break;
+			case 3:
+				imobiliaria.listagemPorCondicao(sc);
+				break;
+			case 4:
+				imobiliaria.modificarImovel(sc);
+				break;
+			case 5:
+				imobiliaria.removerImovel(sc);
+			case 0:
+				break;
+			default:
+				System.out.println("Opção inválida!");
+			}
+		} while(opcao != 0);
+}
 }
